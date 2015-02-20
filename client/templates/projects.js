f3cae951f37dc42ec846c375a70277f1ba2973cc
phouse512 <philiphouse2015@u.northwestern.edu>
2015-02-20 13:25:18 -0600

    adding brief descriptions

--- client/templates/projects.js
+++ client/templates/projects.js
@@ -5,5 +5,9 @@ Template.Projects.helpers({
 
   sig_project: function(sig_id) {
   	return Projects.find({sig: sig_id});
+  },
+
+  learn_more: function(text) {
+  	return text.split(" ").splice(0,30).join(" ") + "...";
   }
 });

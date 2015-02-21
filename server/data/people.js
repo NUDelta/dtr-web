8d5c5a43fc40233ba9328c334cce774c456fbce3
Haoqi Zhang <hqz@mit.edu>
2015-02-20 22:47:02 -0600

    hi

--- server/data/people.js
+++ server/data/people.js
@@ -14,11 +14,25 @@ people=[
 	"description": "<a href=\"http://egerber.mech.northwestern.edu/\">Liz</a> is the Breed Junior Chair of Design in the <a href=\"http://www.mccormick.northwestern.edu\">McCormick School of Engineering</a> and <a href=\"http://www.communication.northwestern.edu\">School of Communication</a> at Northwestern University. She has spent 11 years researching and designing award winning innovation programs for universities and S&amp;P 500 companies nationally and internationally. She has been invited to speak nationally and internationally at non-profit and for-profit institutions, including Facebook, Hewlett-Packard, MIT, and Stanford. Her work has been featured in such venues as Oprah, NPR, ABC, Harvard Business Review, Forbes, and Fast Company, and is currently funded by the National Science Foundation, the Mozilla Foundation, and the MacArthur Foundation."
     },
     {
+	"_id":"eharburg",
+	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2013/11/emily-250x250.jpg",
+	"name": "Emily Harburg",
+	"role": "PhD Student",
+	"description": "<a href=\"http://emilyharburg.com/\">Emily</a> is a Ph.D. Student in the <a href=\"http://tsb.northwestern.edu\">Technology and Social Behavior</a> program at Northwestern University. Emily is interested in building tools to improve motivation, decision-making, innovation, and collaboration. She is currently looking at the effects of using crowfunding to increase self-efficacy. Emily received her B.S. in Social Anthropology with a minor in Psychology from Harvard University. Prior to doctoral studies, Emily worked for Walt Disney Imagineering in the Disney Research behavioral sciences team."
+    },
+    {
 	"_id":"jhibschman",
 	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2014/09/josh.png",
 	"name": "Josh Hibschman",
 	"role": "PhD student",
 	"description": "<a href=\"http://users.eecs.northwestern.edu/~jjh380/\">Josh</a> is a Computer Science PhD student at Northwestern University. His research interests include human computation, crowdsourcing, and social computing. He holds an M.S. in Computer Science from DePaul University, and a B.S. in Computer Science from Taylor University. Before starting his PhD, Josh engineered scalable web solutions for clients in healthcare and finance at Accenture and Slalom Consulting. When not busy in the lab, Josh enjoys sailing, cycling, and discussing serious scientific answers to <a href=\"https://whatif.xkcd.com/book/\">absurd hypothetical questions</a>."
+    },
+    {
+	"_id":"scambo",
+	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2015/02/scott.jpg",
+	"name": "Scott Cambo",
+	"role": "PhD Student",
+	"description": "Scott is a PhD student in the Technology and Social Behavior program.  He completed his undergraduate degree in Information Science (minor in Cognitive Science) at Cornell University and was a 2014 Data Science For Social Good Fellow.  His research interests include ubiquitous computing, mobile computing, HCI, social computing, personal informatics and generally any kind of research that helps to people to gain actionable knowledge about themselves and the world we live in through technology."
     },
     {
 	"_id":"ykim",
@@ -28,25 +42,18 @@ people=[
 	"description": "<a href=\"http://www.yongsungkim.com\">Yongsung</a> is a first year Ph.D. Student in the Technology and Social Behavior program at Northwestern University. Yongsung is interested in understanding user interactions and motivations in existing online communities such as Q&A and crowdsourcing, as well as designing new systems. Yongsung received his M.Sc in Computer Science from Swiss Federal Institute of Technology Lausanne (EPFL) and B.Eng in Computer Science from Tsinghua University"
     },
     {
-	"_id":"zallen",
-	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2014/09/zak.jpg",
-	"name": "Zak Allen",
+	"_id":"ahollenbeck",
+	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2014/09/alex.jpg",
+	"name": "Alex Hollenbeck",
 	"role": "Undergrad Researcher",
-	"description": "Zak is an undergraduate student at Northwestern University majoring in Computer Science in the McCormick School of Engineering. He is minoring in business and also plays on the soccer team. Zak is interested in crowdsourcing, user-centered design, and human-computer interaction. His research includes exploring how emotional tags can be linked to Instagram photos."
+	"description": "Alex is a fourth year undergraduate student studying Computer Science at Northwestern University. His interests lie at the intersection of front-end engineering and user experience design. Professionally, he enjoys web interface prototyping and pattern development. His current research interests include exploring ways to foster participation in open-source development and other collaborative software endeavors."
     },
     {
-	"_id":"schan",
-	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2013/11/stephen-250x250.jpg",
-	"name": "Stephen Chan",
+	"_id":"brothman",
+	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2014/09/ben.jpg",
+	"name": "Ben Rothman",
 	"role": "Undergrad Researcher",
-	"description": "Stephen is an undergraduate Computer Science student. He is currently working on a project on low-effort crowdsourcing in physical spaces."
-    },   
-    {
-	"_id":"kchen",
-	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2014/10/kevin.jpg",
-	"name": "Kevin Chen",
-	"role": "Undergrad Researcher",
-	"description": "Kevin is an undergraduate student at Northwestern University majoring in Computer Science in the McCormick School of Engineering. He is interested in sociotechnical connections, crowdsourcing, artificial intelligence, and algorithms. Kevin's research explores new prototyping interactions made available by Google Glass."
+	"description": "Ben is a fifth year undergraduate completing a co-op and double majoring in Computer Science and Applied Math. He is a full stack web developer interested in interfaces and is unsure of his research direction."
     },
     {
 	"_id":"cgrief",
@@ -56,32 +63,18 @@ people=[
 	"description": "Corey is an undergraduate student at Northwestern University majoring in Computer Science in the McCormick School of Engineering. Corey is in the Marching and Concert Bands. He is interested in artificial intelligence and crowdsourcing. His research involves creating an application to facilitate and examine the sharing of secrets."
     },
     {
-	"_id":"ahollenbeck",
-	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2014/09/alex.jpg",
-	"name": "Alex Hollenbeck",
+	"_id": "ckim",
+	"photoLink" : "http://delta.northwestern.edu/wordpress/wp-content/uploads/2015/02/christina.jpg",
+	"name": "Christina Kim",
 	"role": "Undergrad Researcher",
-	"description": "Alex is a fourth year undergraduate student studying Computer Science at Northwestern University. His interests lie at the intersection of front-end engineering and user experience design. Professionally, he enjoys web interface prototyping and pattern development. His current research interests include exploring ways to foster participation in open-source development and other collaborative software endeavors."
+	"description": "Christina is a undergraduate student at Northwestern University studying Computer Science in the McCormick School of Engineering. She is interested in human-centered design, online communities, and social computing. As Vice President of Services of Northwestern's Associated Student Government, Christina works to create web apps and intiatives for Northwestern's undergraduates."
     },
     {
-	"_id":"phouse",
-	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2014/10/philip.jpg",
-	"name": "Philip House",
+	"_id": "favino",
+	"photoLink" : "http://delta.northwestern.edu/wordpress/wp-content/uploads/2015/02/frank.jpg",
+	"name": "Frank Avino",
 	"role": "Undergrad Researcher",
-	"description": "Philip is an undergraduate student at Northwestern University studying Computer Science in the McCormick School of Engineering. His primary interests lie in crowdsourcing and seeing how the web can change how people interact and learn. His current research involves exploring how online education can simultaneously teach and contribute meaningful data."
-    },
-    {
-	"_id":"lmaliakal",
-	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2014/10/leesha.png",
-	"name": "Leesha Maliakal",
-	"role": "Undergrad Researcher",
-	"description": "Leesha is a Computer Science undergraduate student at Northwestern University. She is interested in human computer interaction, tangible interface design, crowdsourcing, game design, mobile development, and artificial intelligence. Her current research explores physical crowdsourcing, specifically group activities that encourage physical fitness."
-    },
-    {
-	"_id":"brothman",
-	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2014/09/ben.jpg",
-	"name": "Ben Rothman",
-	"role": "Undergrad Researcher",
-	"description": "Ben is a fifth year undergraduate completing a co-op and double majoring in Computer Science and Applied Math. He is a full stack web developer interested in interfaces and is unsure of his research direction."
+	"description": "Frank Avino is an undergraduate Computer Science student in the McCormick School of Engineering and Applied Science, graduating in June 2015. His lifetime of experience with playing the game of hockey has lead him to giving back to the game through research. His current research seeks to solve the problem of hockey coaches being unable to provide enough thorough, factual feedback to each of his or her players during and after each of the team's games."
     },
     {
 	"_id":"jruffer",
@@ -98,6 +91,20 @@ people=[
 	"description": "Kalina Silverman is an undergraduate majoring in Broadcast Journalism at the Medill School of Journalism. As the co-founder and president of Northwestern's Mixed Race Student Coalition, she is interested in further finding ways to bridge connections between individuals of diverse backgrounds on a global scale. She is currently working on a platform that inspires deeper, meaningful interactions in every day life. "
     },
     {
+	"_id":"kchen",
+	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2014/10/kevin.jpg",
+	"name": "Kevin Chen",
+	"role": "Undergrad Researcher",
+	"description": "Kevin is an undergraduate student at Northwestern University majoring in Computer Science in the McCormick School of Engineering. He is interested in sociotechnical connections, crowdsourcing, artificial intelligence, and algorithms. Kevin's research explores new prototyping interactions made available by Google Glass."
+    },
+    {
+	"_id":"lmaliakal",
+	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2014/10/leesha.png",
+	"name": "Leesha Maliakal",
+	"role": "Undergrad Researcher",
+	"description": "Leesha is a Computer Science undergraduate student at Northwestern University. She is interested in human computer interaction, tangible interface design, crowdsourcing, game design, mobile development, and artificial intelligence. Her current research explores physical crowdsourcing, specifically group activities that encourage physical fitness."
+    },
+    {
 	"_id":"nzhu",
 	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2013/11/nicole-250x250.jpg",
 	"name": "Nicole Zhu",
@@ -105,32 +112,26 @@ people=[
 	"description": "Nicole is an undergraduate student at Northwestern University majoring in English and Computer Science. She works on projects with the student group Design for America and does web design for the news site North by Northwestern. Her interests include crowdsourcing, interactive news/storytelling, and social design. Her research involves developing a mobile app to promote low-effort crowdsourcing through physical gestures and mobile sensors."
     },
     {
-	"_id":"eharburg",
-	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2013/11/emily-250x250.jpg",
-	"name": "Emily Harburg",
-	"role": "PhD Student",
-	"description": "<a href=\"http://emilyharburg.com/\">Emily</a> is a Ph.D. Student in the <a href=\"http://tsb.northwestern.edu\">Technology and Social Behavior</a> program at Northwestern University. Emily is interested in building tools to improve motivation, decision-making, innovation, and collaboration. She is currently looking at the effects of using crowfunding to increase self-efficacy. Emily received her B.S. in Social Anthropology with a minor in Psychology from Harvard University. Prior to doctoral studies, Emily worked for Walt Disney Imagineering in the Disney Research behavioral sciences team."
-    },
-    {
-	"_id":"scambo",
-	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2015/02/scott.jpg",
-	"name": "Scott Cambo",
-	"role": "PhD Student",
-	"description": "Scott is a PhD student in the Technology and Social Behavior program.  He completed his undergraduate degree in Information Science (minor in Cognitive Science) at Cornell University and was a 2014 Data Science For Social Good Fellow.  His research interests include ubiquitous computing, mobile computing, HCI, social computing, personal informatics and generally any kind of research that helps to people to gain actionable knowledge about themselves and the world we live in through technology."
-    },
-    {
-	"_id": "ckim",
-	"photoLink" : "http://delta.northwestern.edu/wordpress/wp-content/uploads/2015/02/christina.jpg",
-	"name": "Christina Kim",
+	"_id":"phouse",
+	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2014/10/philip.jpg",
+	"name": "Philip House",
 	"role": "Undergrad Researcher",
-	"description": "Christina is a undergraduate student at Northwestern University studying Computer Science in the McCormick School of Engineering. She is interested in human-centered design, online communities, and social computing. As Vice President of Services of Northwestern's Associated Student Government, Christina works to create web apps and intiatives for Northwestern's undergraduates."
+	"description": "Philip is an undergraduate student at Northwestern University studying Computer Science in the McCormick School of Engineering. His primary interests lie in crowdsourcing and seeing how the web can change how people interact and learn. His current research involves exploring how online education can simultaneously teach and contribute meaningful data."
     },
     {
-	"_id": "favino",
-	"photoLink" : "http://delta.northwestern.edu/wordpress/wp-content/uploads/2015/02/frank.jpg",
-	"name": "Frank Avino",
+	"_id":"schan",
+	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2013/11/stephen-250x250.jpg",
+	"name": "Stephen Chan",
 	"role": "Undergrad Researcher",
-	"description": "Frank Avino is an undergraduate Computer Science student in the McCormick School of Engineering and Applied Science, graduating in June 2015. His lifetime of experience with playing the game of hockey has lead him to giving back to the game through research. His current research seeks to solve the problem of hockey coaches being unable to provide enough thorough, factual feedback to each of his or her players during and after each of the team's games."
-    }
+	"description": "Stephen is an undergraduate Computer Science student. He is currently working on a project on low-effort crowdsourcing in physical spaces."
+    },   
+    {
+	"_id":"zallen",
+	"photoLink":"http://delta.northwestern.edu/wordpress/wp-content/uploads/2014/09/zak.jpg",
+	"name": "Zak Allen",
+	"role": "Undergrad Researcher",
+	"description": "Zak is an undergraduate student at Northwestern University majoring in Computer Science in the McCormick School of Engineering. He is minoring in business and also plays on the soccer team. Zak is interested in crowdsourcing, user-centered design, and human-computer interaction. His research includes exploring how emotional tags can be linked to Instagram photos."
+    },
+
 ];
 

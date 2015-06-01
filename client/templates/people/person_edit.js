134c6b0d736f9900d52373a1a0be8b34b12764b7
aspin <tsunami70875@gmail.com>
2015-05-31 19:22:01 -0500

    changed one icon

--- client/templates/people/person_edit.js
+++ client/templates/people/person_edit.js
@@ -7,7 +7,7 @@ Template.personEdit.events({
 
 		People.update(this.person._id, {$set: update});
 	},
-    'click .projects-list-item .glyphicon-minus': function (e) {
+    'click .projects-list-item .glyphicon-pencil': function (e) {
         var id = e.currentTarget.getAttribute('data-toggle');
         $('#' + id).slideToggle(600);
     },

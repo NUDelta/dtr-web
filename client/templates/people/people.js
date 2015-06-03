de824f8563789e997b0d03b52963f95c099b28bb
aspin <tsunami70875@gmail.com>
2015-06-03 18:04:01 -0500

    fixed sort order of roster

--- client/templates/people/people.js
+++ client/templates/people/people.js
@@ -1,8 +1,33 @@
 Template.People.helpers({
   people: function() {
-    return People.find();
+    return People.find().fetch().sort(function(x, y) {
+        if (computeWorth(y) !== computeWorth(x)) {
+            return computeWorth(y) - computeWorth(x);
+        }
+        else {
+            return x.name.toLowerCase() > y.name.toLowerCase();
+        }
+    });
   }
 });
 
-
-
+function computeWorth(person) {
+    if (person._id === 'hzhang') {
+        return 10;
+    }
+    else if (person._id === 'lgerber') {
+        return 9;
+    }
+    else if (person.role.toLowerCase() === 'phd student') {
+        return 8;
+    }
+    else if (person.role.toLowerCase() === 'undergrad researcher') {
+        return 7;
+    }
+    else if (person.role.toLowerCase() === 'alum') {
+        return 6;
+    }
+    else {
+        return 0;
+    }
+}

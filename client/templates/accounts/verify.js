f3942ad11961dea726f27b24b7185b73e127daf8
Fardeem Munir <munirfardeem@gmail.com>
2021-11-18 11:36:15 -0600

    Replace meteor with next

--- client/templates/accounts/verify.js
+++
@@ -1,9 +0,0 @@
-Accounts.onEmailVerificationLink(function(token, done) {
-  Accounts.verifyEmail(token, function(err) {
-    if (typeof err === 'undefined') {
-      alert("Your email has been verified. Thank you.")
-    } else {
-      console.log("Something went wrong with verifying the email. Please contact the administrator.")
-    }
-  })
-});
--- client/templates/people/person.js
+++
@@ -1,6 +0,0 @@
-Template.Person.helpers({
-  getProfilePic: function() {
-    return this.photoLink !== "" ? this.photoLink : "/images/default-pic.png";
-  }
-});
-

1704e78f11f4461a7543db70412386a0c69eef20
Fardeem Munir <munirfardeem@gmail.com>
2021-12-20 00:55:35 -0600

    Individual project pages

--- lib/airtable.ts
+++ lib/airtable.ts
@@ -1,7 +1,5 @@
 import Airtable from "airtable";
 
-console.log(process.env.AIRTABLE_API_KEY);
-
 Airtable.configure({
   endpointUrl: "https://api.airtable.com",
   apiKey: process.env.AIRTABLE_API_KEY,
@@ -54,11 +52,17 @@ export type Project = {
   id: string;
   name: string;
   description: string | null;
+  members: string[];
+  images: ProjectImages;
+  publications: ProjectPublication[];
 };
 
-export async function getProject(projectId: string): Promise<Project> {
+export async function getProject(
+  projectId: string,
+  getAllData = false
+): Promise<Project> {
   return new Promise((resolve, reject) => {
-    base("Projects").find(projectId, function (err, record) {
+    base("Projects").find(projectId, async function (err, record) {
       if (err) {
         reject(err);
         return;
@@ -68,10 +72,36 @@ export async function getProject(projectId: string): Promise<Project> {
         return;
       }
 
-      resolve({
+      const partialProject = {
         id: record.id,
         name: record.get("name") as string,
         description: (record.get("description") as string) ?? null,
+        members: record.get("members") as string[],
+      };
+
+      if (!getAllData) {
+        resolve({
+          ...partialProject,
+          images: {
+            bannerImageUrl: null,
+            explainerImages: [],
+          },
+          publications: [],
+        });
+
+        return;
+      }
+
+      const imageDocId = record.get("images") as string[];
+      const images = await fetchProjectImages(imageDocId[0]);
+
+      const publicationDocId = record.get("publications") as string[];
+      const publications = await fetchPublications(publicationDocId[0]);
+
+      resolve({
+        ...partialProject,
+        images,
+        publications,
       });
     });
   });
@@ -124,6 +154,120 @@ export async function fetchSigs(): Promise<SIG[]> {
             return;
           }
           resolve(results);
+        }
+      );
+  });
+}
+
+type ProjectImages = {
+  bannerImageUrl: string | null;
+  explainerImages: {
+    url: string;
+    description: string;
+  }[];
+};
+
+export async function fetchProjectImages(
+  imageDocId: string
+): Promise<ProjectImages> {
+  return new Promise((resolve, reject) => {
+    base("Project Images").find(imageDocId, function (err, record) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      if (!record) {
+        reject("Project Images not found");
+        return;
+      }
+
+      const explainerImages: ProjectImages["explainerImages"] = [];
+
+      [1, 2, 3, 4, 5].map((i) => {
+        const imageUrl = record.get(`image_${i}_url`) as string;
+        const description = record.get(`image_${i}_description`) as string;
+
+        if (imageUrl && description) {
+          explainerImages.push({
+            url: imageUrl,
+            description,
+          });
+        }
+      });
+
+      resolve({
+        bannerImageUrl: (record.get("banner_image_url") as string) ?? null,
+        explainerImages,
+      });
+    });
+  });
+}
+
+type ProjectPublication = {
+  id: string;
+  name: string;
+  conference: string;
+  url: string;
+};
+
+export async function fetchPublications(
+  publicationDocId: string
+): Promise<ProjectPublication[]> {
+  return new Promise((resolve, reject) => {
+    base("Project Publications").find(publicationDocId, function (err, record) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      if (!record) {
+        reject("Project Publication not found");
+        return;
+      }
+
+      const publications: ProjectPublication[] = [];
+
+      [1, 2, 3, 4, 5].map((i) => {
+        const name = record.get(`publication_${i}_name`) as string;
+        const conference = record.get(`publication_${i}_conference`) as string;
+        const url = record.get(`publication_${i}_url`) as string;
+
+        if (name && conference && url) {
+          publications.push({
+            id: `${record.id}-publication-${i}`,
+            name,
+            conference,
+            url,
+          });
+        }
+      });
+
+      resolve(publications);
+    });
+  });
+}
+
+export async function getAllProjectIds(): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+    const projectIds: string[] = [];
+
+    base("Projects")
+      .select({
+        view: "Grid view",
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          for (const record of records) {
+            projectIds.push(record.id);
+          }
+
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(projectIds);
         }
       );
   });
---
+++ pages/projects/[id].tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import Link from "next/link";
+import Header from "../../components/shared/Header";
+import Container from "../../components/shared/Container";
+import { getAllProjectIds, getProject, Project } from "../../lib/airtable";
+import { GetStaticPaths, GetStaticProps } from "next";
+
+interface IndividualProjectPageProps {
+  project: Project;
+}
+
+export default function IndividualProjectPage({
+  project,
+}: IndividualProjectPageProps): JSX.Element {
+  return (
+    <main>
+      <Header />
+
+      <Container className="mt-20 max-w-5xl">
+        <div className="bg-gray-50 p-4">
+          <h2 className="font-semibold text-2xl mb-4">{project.name}</h2>
+
+          {project.images.bannerImageUrl && (
+            <img
+              src={project.images.bannerImageUrl}
+              className="w-full"
+              alt={project.name}
+            />
+          )}
+
+          <div className="prose-lg my-8">{project.description}</div>
+
+          <div className="grid grid-cols-2 gap-8 mb-12">
+            {project.images.explainerImages.map((img, i) => (
+              <div key={`img-${i}`}>
+                <img
+                  src={img.url}
+                  className="w-full mb-4"
+                  alt={`${project.name} ${i}`}
+                />
+                <p className="text-sm">{img.description}</p>
+              </div>
+            ))}
+          </div>
+
+          {project.publications.length > 0 && (
+            <div className="w-96">
+              <h2 className="font-bold text-2xl mb-2 pb-2 border-b border-black">
+                Publications
+              </h2>
+
+              <ul className="font-medium prose mb-12">
+                {project.publications.map((publication) => (
+                  <li key={publication.id}>
+                    <a href={publication.url}>{publication.name}</a>,{" "}
+                    {publication.conference}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+          <div className="w-96">
+            <h2 className="font-bold text-2xl mb-2 pb-2 border-b border-black">
+              Team
+            </h2>
+
+            <ul className="font-medium">
+              {project.members.map((member) => (
+                <li key={member}>{member}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </Container>
+    </main>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async (query) => {
+  const project = await getProject(query.params?.id as string, true);
+
+  return {
+    props: {
+      project,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const projectIds = await getAllProjectIds();
+
+  return {
+    paths: projectIds.map((projectId) => ({
+      params: {
+        id: projectId,
+      },
+    })),
+    fallback: true,
+  };
+};

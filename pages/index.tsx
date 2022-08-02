ce1ff41fe44ce556293057492927aafa516ee375
Kapil Garg <kapil1garg@gmail.com>
2022-08-02 03:15:53 -0500

    adding banner with links to DTR documentary and annual letters to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Header from "../components/shared/Header";
 import Container from "../components/shared/Container";
 import Slides from "../components/home/Slides";
 import HomeIntro from "../components/home/HomeIntro";
+import Link from "next/link";
 
 export default function Home(): JSX.Element {
   return (
@@ -17,6 +18,28 @@ export default function Home(): JSX.Element {
           <HomeIntro />
         </div>
       </Container>
+
+        <Container className="mt-4">
+            <div className="w-full bg-yellow pt-2 pb-2 rounded-lg">
+                <p className="text-center text-black">
+                    Want to learn more? Browse this website, watch the {" "}
+                    <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://forward.movie"
+                    >
+                        <span className="link link-underline link-underline-black text-black">DTR Documentary</span>
+                    </a>, {" "}
+                    and read the{" "}
+                    <Link href="/letters">
+                        <a>
+                            <span className="link link-underline link-underline-black text-black">DTR Annual Letters</span>
+                        </a>
+                    </Link>
+                    .
+                </p>
+            </div>
+        </Container>
     </div>
   );
 }
